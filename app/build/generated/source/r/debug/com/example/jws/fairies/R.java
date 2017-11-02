2e9c535b5fe763e9abedb7a83c1e32fa530d635d
ksh001479 <ksh001479@daum.net>
2017-11-02 22:06:10 +0900

    아이콘 변경완료 APK생성

--- app/build/generated/source/r/debug/com/example/jws/fairies/R.java
+++ app/build/generated/source/r/debug/com/example/jws/fairies/R.java
@@ -2908,7 +2908,10 @@ public final class R {
   }
   public static final class mipmap {
     public static final int ic_launcher=0x7f0a0000;
-    public static final int ic_launcher_round=0x7f0a0001;
+    public static final int ic_launcher_foreground=0x7f0a0001;
+    public static final int ic_launcher_round=0x7f0a0002;
+    public static final int icon=0x7f0a0003;
+    public static final int icon_round=0x7f0a0004;
   }
   public static final class string {
     public static final int abc_action_bar_home_description=0x7f0b0000;
